c30f9036fecdb200242302a89f5bfd4f41ee0efc
BrandenXia <brandenxia@163.com>
2023-02-19 20:01:48 -0800

    docs: Add annotation for UserService

--- src/main/java/com/simpleforum/simpleforum/service/UserService.java
+++ src/main/java/com/simpleforum/simpleforum/service/UserService.java
@@ -3,7 +3,21 @@ package com.simpleforum.simpleforum.service;
 import com.simpleforum.simpleforum.entity.User;
 
 public interface UserService {
+    /**
+     * Register a new user
+     * @param username username
+     * @param password password
+     * @param email email
+     * @param phoneNumber phone number
+     * @return the new user
+     */
     User register(String username, String password, String email, String phoneNumber);
 
+    /**
+     * Login a user
+     * @param username username
+     * @param password password
+     * @return success or not
+     */
     Boolean login(String username, String password);
 }
