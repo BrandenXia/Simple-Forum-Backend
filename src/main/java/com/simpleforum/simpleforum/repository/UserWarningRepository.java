36338510c2a371e0f10a7411ace7e57ee86ded1d
BrandenXia <brandenxia@163.com>
2023-02-25 15:50:49 -0800

    refactor(repository): Use JpaRepository instead of writing the code myself
    - Make UserWarningRepository extends JpaRepository

--- src/main/java/com/simpleforum/simpleforum/repository/UserWarningRepository.java
+++ src/main/java/com/simpleforum/simpleforum/repository/UserWarningRepository.java
@@ -2,38 +2,13 @@ package com.simpleforum.simpleforum.repository;
 
 import com.simpleforum.simpleforum.entity.User;
 import com.simpleforum.simpleforum.entity.UserWarning;
-import com.simpleforum.simpleforum.exception.DAOException;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
-public interface UserWarningRepository {
-    /**
-     * Create a new user warning
-     * @param userWarning the user warning to create
-     * @return the created user warning
-     */
-    @Transactional
-    UserWarning createWarning(UserWarning userWarning);
+public interface UserWarningRepository extends JpaRepository<UserWarning, String> {
+    List<UserWarning> findByUser(User user);
 
-    @Transactional
-    void deleteWarning(UserWarning userWarning);
-
-    UserWarning getUserWarningById(String id) throws DAOException;
-
-    List<UserWarning> getUserWarningsByUser(User user) throws DAOException;
-
-    List<UserWarning> getUserWarningsByUser(User user, int limit) throws DAOException;
-
-    List<UserWarning> getUserWarningsByModerator(User moderator) throws DAOException;
-
-    List<UserWarning> getUserWarningsByModerator(User moderator, int limit) throws DAOException;
-
-    List<UserWarning> getUserWarningsByDate(long date) throws DAOException;
-
-    List<UserWarning> getUserWarningsByDate(long date, int limit) throws DAOException;
-
-    List<UserWarning> getUserWarningsByDate(long date1, long date2) throws DAOException;
-
-    List<UserWarning> getUserWarningsByDate(long date1, long date2, int limit) throws DAOException;
+    List<UserWarning> findByDateBetween(Date date1, Date date2);
 }
