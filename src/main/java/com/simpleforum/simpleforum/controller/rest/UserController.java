69cb0573035b1e53d43bc678a19fe96d7c6af55b
BrandenXia <brandenxia@163.com>
2023-02-26 17:41:26 -0800

    refactor(controller): Rewrite controller to fit in new utils

--- src/main/java/com/simpleforum/simpleforum/controller/rest/UserController.java
+++ src/main/java/com/simpleforum/simpleforum/controller/rest/UserController.java
@@ -25,21 +25,15 @@ public class UserController {
     public ResponseUtils.Response register(@RequestBody UserDTO registerUser) {
         if (registerUser.getUsername() == null || registerUser.getPassword() == null) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("invalid username or password");
+                    .error(400, "invalid username or password");
         }
         User user = userService.register(registerUser.getUsername(), registerUser.getPassword(), registerUser.getEmail(), registerUser.getPhoneNumber());
         if (user == null) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("username or email already exists");
+                    .error(400, "username, email or phoneNumber already exists");
         }
         return ResponseUtils.createResponse()
-                .success()
-                .setCode(200)
-                .setMessage("success");
+                .success();
     }
 
     @PostMapping("/login")
@@ -47,9 +41,7 @@ public class UserController {
         Boolean result = userService.login(user.getUsername(), user.getEmail(), user.getPhoneNumber(), user.getPassword());
         if (!result) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("invalid username, email, phone number or password");
+                    .error(400, "invalid username, email, phone number or password");
         }
         Map<String, String> payload = new HashMap<>();
         payload.put("username", user.getUsername());
@@ -57,8 +49,6 @@ public class UserController {
         String token = JwtUtils.getToken(payload);
         return ResponseUtils.createResponse()
                 .success()
-                .setCode(200)
-                .setMessage("success")
                 .setData(Map.of("token", token));
     }
 
@@ -67,52 +57,36 @@ public class UserController {
         User currentUser = userService.getCurrentUser(token);
         if (currentUser == null) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("invalid token");
+                    .error(400, "invalid token");
         }
         if (!user.getPassword().equals(currentUser.getPassword())) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("invalid password");
+                    .error(400, "invalid password");
         }
         if (user.getUsername().equals(currentUser.getUsername())) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("new username cannot be the same as the old one");
+                    .error(400, "new username cannot be the same as the old one");
         }
         if (user.getEmail().equals(currentUser.getEmail())) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("new email cannot be the same as the old one");
+                    .error(400, "new email cannot be the same as the old one");
         }
         if (user.getNewPassword().equals(currentUser.getPassword())) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("new password cannot be the same as the old one");
+                    .error(400, "new password cannot be the same as the old one");
         }
         if (user.getPhoneNumber().equals(currentUser.getPhoneNumber())) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("new phone number cannot be the same as the old one");
+                    .error(400, "new phone number cannot be the same as the old one");
         }
         try {
             userService.updateUser(currentUser, user.getUsername(), user.getNewPassword(), user.getEmail(), user.getPhoneNumber());
         } catch (Exception e) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage(e.getMessage());
+                    .error(400, e.getMessage());
         }
         return ResponseUtils.createResponse()
-                .success()
-                .setCode(200)
-                .setMessage("success");
+                .success();
     }
 
     @GetMapping("/current")
@@ -120,14 +94,10 @@ public class UserController {
         User user = userService.getCurrentUser(token);
         if (user == null) {
             return ResponseUtils.createResponse()
-                    .error()
-                    .setCode(400)
-                    .setMessage("invalid token");
+                    .error(400, "invalid token");
         }
         return ResponseUtils.createResponse()
                 .success()
-                .setCode(200)
-                .setMessage("success")
                 .setData(Map.of("username", user.getUsername()));
     }
 }
