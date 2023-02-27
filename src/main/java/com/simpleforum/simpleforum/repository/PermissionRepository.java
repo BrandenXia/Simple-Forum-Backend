447ce9bcd05d0ab61de80fbf9430f1f17d32e5aa
BrandenXia <brandenxia@163.com>
2023-02-27 11:07:02 -0800

    feat(repository): Add methods for repository
    - Add `findByName` in PermissionRepository
    - Add `findByName` in
    TopicRepository

--- src/main/java/com/simpleforum/simpleforum/repository/PermissionRepository.java
+++ src/main/java/com/simpleforum/simpleforum/repository/PermissionRepository.java
@@ -4,5 +4,7 @@ import com.simpleforum.simpleforum.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission, String> {
+    Permission findByName(String name);
+
     Boolean existsByName(String name);
 }
--- src/main/java/com/simpleforum/simpleforum/repository/TopicRepository.java
+++ src/main/java/com/simpleforum/simpleforum/repository/TopicRepository.java
@@ -4,6 +4,8 @@ import com.simpleforum.simpleforum.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicRepository extends JpaRepository<Topic, String> {
+    Topic findByName(String name);
+
     Topic findByNameContainingIgnoreCase(String name);
 
     Boolean existsByName(String name);
