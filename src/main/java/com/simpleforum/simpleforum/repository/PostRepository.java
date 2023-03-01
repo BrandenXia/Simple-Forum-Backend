69f11562cd58cf92af76e3feada7fb809e6aa71d
BrandenXia <brandenxia@163.com>
2023-03-01 08:26:37 -0800

    feat(repository): Add `findByTopicAndDeleted` method in PostRepository

--- src/main/java/com/simpleforum/simpleforum/repository/PostRepository.java
+++ src/main/java/com/simpleforum/simpleforum/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.simpleforum.simpleforum.repository;
 
 import com.simpleforum.simpleforum.entity.Post;
+import com.simpleforum.simpleforum.entity.Topic;
 import com.simpleforum.simpleforum.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +11,8 @@ public interface PostRepository extends JpaRepository<Post,String> {
     Page<Post> findByUserOrderByCreatedTime(Pageable pageable, User user);
 
     Page<Post> findByDeleted(Boolean deleted, Pageable pageable);
+
+    Page<Post> findByTopicAndDeleted(Topic topic, Boolean deleted, Pageable pageable);
 
     Page<Post> findByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseAndDeleted(String title, String content, Boolean deleted ,Pageable pageable);
 }
